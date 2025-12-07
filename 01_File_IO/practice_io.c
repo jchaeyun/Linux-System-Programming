29da874ad14b428531820a3ca6c03dfc1750aed4
jchaeyun <jocheyoon@gmail.com>
2025-12-07 18:38:32 +0100

    lseek과 pwrite 비교 위해 mixed.db로 실습

--- 01_File_IO/practice_io.c
+++ 01_File_IO/practice_io.c
@@ -6,62 +6,41 @@
 
 int main()
 {
+    // lseek(fd,이동할 거리,기준점):파일에서 커서(위치)를 순간 이동시키는 함수
+    // SEEK_SET:맨 앞 기준(절대이동)
+    // SEEK_CUR:현재 기준(상대이동)
+    // SEEK_END:맨 끝 기준
     int fd;
-    const char *data1 = "START";
-    const char *data2 = "MIDDLE";
-    const char *data3 = "END";
 
-    // 1단계:파일 열기(open)
-    // O_RDWR:읽기/쓰기 모드로 열기, O_RDONLY (읽기만), O_WRONLY (쓰기만)
-    // O_CREAT:파일이 없으면 새로 생성
-    // O_TRUNC:파일이 이미 있다면 내용을 싹 지우고(0으로) 시작
-    // 0644:파일 권한(나=읽기/쓰기,그룹:읽기,타인:읽기)
-    // fd로 들어오는 반환값은 정수(실패 시 -1 반환), 연 파일을 fd(숫자)로 부르는 것.
+    // 1.파일 열기
+    // 이번엔 mixed.db 라는 파일을 만듦
 
-    printf("[Process] test.db 파일을 엽니다...\n");
-    fd = open("test.db", O_RDWR | O_CREAT | O_TRUNC, 0644);
-
+    fd = open("mixed.db", O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd == -1)
     {
-        perror("파일 열기 실패"); // 에러 발생 시 원인 출력
+        perror("파일 열기 실패");
         exit(1);
     }
 
-    // 2단계:데이터 쓰기(pwrite 사용)->lseek 대신 사용!
-    // pwrite(파일디스크립터,데이터,데이터길이,오프셋(위치));
-    // 이 함수는 커서를 움직이지 않고 지정된 위치에 바로 꽂아 넣음
+    //[step 1] 일반 write: "AAAA" 쓰기(커서가 쓰면서 움직임),현재 커서 위치부터 숫자만큼 쓰고 이동(3이었으면 AAA씀)
+    // 현재 커서:0
+    // 쓰고 난 후 커서 예상:4
+    write(fd, "AAAA", 4);
+    // lseek(fd,0,SEEK_CUR):현재위치(SEEL_CUR)에서 0칸만큼 이동해라, 반환값:파일의 맨 처음부터 현재 커서까지의 거리(바이트수) 반환
+    printf("1. 'AAAA' 작성 완료. 현재 커서 위치: %ld\n", lseek(fd, 0, SEEK_CUR));
 
-    // 1.0번지(파일의 맨 앞)에 데이터 쓰기
-    if (pwrite(fd, data1, strlen(data1), 0) == -1)
-    {
-        perror("0번지 쓰기 실패");
-        close(fd);
-        exit(1);
-    }
+    //[step 2] pwrite:저 멀리 100번지에 "ZZZZ" 쓰기(4아니고 3으로 쓰면 ZZZ만 100번지에 써짐)
+    // pwrite은 커서를 건드리지 않아야함
+    // 쓰고난 후 커서 예상:여전히 4여야함(이게 핵심)
+    pwrite(fd, "ZZZZ", 4, 100);
+    printf("2. 100번지에 'ZZZZ' (pwrite) 작성 완료.\n");
+    printf(" ->pwrite 직후 커서 위치 확인:%ld (변하면 안됨)\n", lseek(fd, 0, SEEK_CUR));
 
-    printf("[Success] 0번지에 \"%s\" 작성 완료.\n", data1);
-
-    // 2.4000번지에 데이터 쓰기(중간에 구멍 생김)
-    if (pwrite(fd, data2, strlen(data2), 4000) == -1)
-    {
-        perror("4000번지 쓰기 실패");
-        exit(1);
-    }
-
-    printf("[Success] 4000번지에 \"%s\" 작성 완료.\n", data2);
-
-    // 3.8000번지에 데이터 쓰기(더 큰 구멍 생성)
-    if (pwrite(fd, data3, strlen(data3), 8000) == -1)
-    {
-        perror("8000번지 쓰기 실패");
-        close(fd);
-        exit(1);
-    }
-
-    printf("[Success] 8000번지에 \"%s\" 작성 완료\n", data3);
+    //[step 3] 다시 일반 write:"BBBB" 쓰기
+    // 만약 커서가 4에 그대로 있다면 "AAAA" 바로 뒤에 "BBBB"가 붙어야함
+    write(fd, "BBBB", 4);
+    printf("3. 'BBBB' 작성 완료. 현재  커서 위치:%ld\n", lseek(fd, 0, SEEK_CUR));
 
     close(fd);
-    printf("[Completed] 모든 작업이 끝났습니다. 'hexdump -C test.db'로 확인해보세요\n");
-
     return 0;
 }
